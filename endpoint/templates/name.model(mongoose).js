2390a1197f667a24c695bf047f1ae08d0b4a9d08
Fabio Lemos Elizandro <fabio@elizandro.com.br>
2015-04-24 16:12:06 -0300

    Correções

--- endpoint/templates/name.model(mongoose).js
+++ endpoint/templates/name.model(mongoose).js
@@ -2,14 +2,23 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var validator = require('node-mongoose-validator');
 
 var <%= classedName %>Schema = new Schema({
   name: String,
   info: String
 });
 
+/**
+ * Plugins
+ */
 <%= classedName %>Schema.plugin(require('mongoose-created-at'));
 <%= classedName %>Schema.plugin(require('mongoose-updated-at'));
 <%= classedName %>Schema.plugin(require('mongoose-delete'));
+
+/**
+ * Validations
+ */
+<%= classedName %>Schema.path('name').validate(validator.notEmpty(), 'Nome não pode ficar em branco');
 
 module.exports = mongoose.model('<%= classedName %>', <%= classedName %>Schema);
